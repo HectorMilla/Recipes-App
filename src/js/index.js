b12ea44ec6b3592d0a443c3da6ba0ebdb9fc708c
hector galicia milla <hectormilla8@gmail.com>
2020-09-30 19:33:56 -0400

    added functionality to list of search results in the view model

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,34 @@
 import Search from "./jsModels/Search";
+import * as searchView from "./views/searchView";
+import { elements } from "./views/base";
+/* Global state of the app
+ * - Search Object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
 
-const search = new Search("pizza");
-console.log(search.getResults());
+const controlSearch = async () => {
+  // 1) Get query from view
+  const query = searchView.getInput();
+
+  if (query) {
+    // 2) Newsearch object and add to state
+    state.search = new Search(query);
+  }
+  // 3) Prepare UI for results
+  searchView.clearInput();
+  searchView.clearResults();
+
+  //4) Search for recipes
+  await state.search.getResults();
+
+  // 5) Render results on UI
+  searchView.renderResults(state.search.result);
+};
+
+elements.searchForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
